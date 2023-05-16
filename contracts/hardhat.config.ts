6946d2c99a016577812101ed4215744303d6590d
Wil-lab07 <79161142+Wil-lab07@users.noreply.github.com>
2023-05-16 18:52:18 +0700

    fix: setup testing and deployment config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,36 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      }
+    }
+  },
+  networks: {
+    polygonMumbai: {
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: 
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.MUMBAI_API_KEY || '',
+    }
+  },
+  mocha: {
+    timeout: process.env.TIMEOUT || 1000000000000,
+  }
 };
 
 export default config;
--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,23 +1,23 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Base = await ethers.getContractFactory('Trace');
+  
+  // Menginput akun pengguna sehingga pada saat deployment telah dilakukan,
+  // akun pengguna akan secara langsung diberi akses sesuai dengan jenis akses yang telah ditetapkan
+  const base = await Base.deploy(
+    '0x430c0c0a0646a2c17DE69b659c18693C5Ea820cF', // RPH
+    '0xD857b46DEC8F4F782Be29Ff1ad0bbE1315C9840C', // Distributor
+    '0xD857b46DEC8F4F782Be29Ff1ad0bbE1315C9840C'  // Rumah_Makan
+  );
+  await base.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  console.log('Smart contract is deployed to:', base.address);
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `npx hardhat verify --network mumbai --constructor-args scripts/argument.js ${base.address}`,
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
